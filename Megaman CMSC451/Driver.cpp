18df9a04e3d4fec11cf5190ac8322a7418b920ee
Nelson Chen <nchen1989@gmail.com>
2013-04-25 16:52:18 -0400

    base set up

---
+++ Megaman CMSC451/Driver.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>						// C++ standard definitions
+#include <iostream>						// I/O stream
+#include <GL/glut.h>                    // GLUT
+#include <GL/glu.h>                     // GLU
+#include <GL/gl.h>                      // OpenGL
+
+int main(int argc, char **argv) 
+{
+	// general initializations
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 400);
+	glutCreateWindow("Megaman CMSC451");
+
+	// register callbacks
+	glutReshapeFunc(); // window reshape callback
+	glutDisplayFunc(); // (re)display callback
+	glutIdleFunc(); // incremental update 
+	glutIgnoreKeyRepeat(); // ignore key repeat when holding key down
+	glutMouseFunc(); // process mouse button push/release
+	glutMotionFunc(); // process mouse dragging motion
+	glutKeyboardFunc(); // process standard key clicks
+	glutSpecialFunc(); // process special key pressed
+	glutSpecialUpFunc(); // process special key release
+
+	// OpenGL init
+	glEnable(GL_DEPTH_TEST);
+
+	// enter GLUT event processing cycle
+	glutMainLoop();
+
+	return 0; // this is just to keep the compiler happy
+}
